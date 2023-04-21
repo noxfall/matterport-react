22acfef2d1779f7cf423a432fd365524eee9f6eb
Kain Noxfall <kain.noxfall@gmail.com>
2023-04-21 18:20:22 +0200

    Add matterport sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import { MpSdk, setupSdk } from '@matterport/sdk';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [sdk, setSdk] = useState();
+  const [horizontal, setHorizontal] = useState(45);
+  const [vertical, setVertical] = useState(15);
+  const container = useRef(null);
+  let started = false;
+
+  useEffect(() => {
+    if (!started && container.current) {
+      started = true;
+      setupSdk(import.meta.env.VITE_MP_SDK, {
+        container: container.current,
+        space: "j4RZx7ZGM6T",
+        iframeQueryParams: { qs: 1 },
+      }).then(setSdk);
+    }
+  }, []);
+
+  const rotate = () => {
+    sdk?.Camera.rotate(horizontal, vertical);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="app">
+      <div className="container" ref={container}></div>
 
-export default App
+      <div className="button-wrap">
+        <label>
+          <span>Horizontal rotation</span>
+          <input
+            type="number"
+            onInput={(e) => setHorizontal(parseFloat(e.target.value))}
+            value={horizontal}
+          />
+        </label>
+        <label>
+          <span>Vertical rotation</span>
+          <input
+            type="number"
+            onInput={(e) => setVertical(parseFloat(e.target.value))}
+            value={vertical}
+          />
+        </label>
+        <button onClick={rotate}>Rotate</button>
+      </div>
+    </main>
+  );
+};
+
+export default App;
